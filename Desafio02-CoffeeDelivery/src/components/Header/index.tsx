6ab1be3516ee8a8b9f8392fe79eeed9e074b1bc7
Emilio <emiliosneto13@gmail.com>
2023-01-03 19:15:05 -0300

    CoffeeDelivery: Finish header Show item count in cart button and set header position to sticky

--- Desafio02-CoffeeDelivery/src/components/Header/index.tsx
+++ Desafio02-CoffeeDelivery/src/components/Header/index.tsx
@@ -1,10 +1,18 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { MapPin, ShoppingCart } from "phosphor-react";
 import logo from "../../assets/logo.svg";
 
 import { HeaderContainer, Location, CheckoutButton } from "./styles";
+import { CartContext } from "../../contexts/CartContext";
 
 export function Header() {
+  const { items } = useContext(CartContext);
+
+  const itemsAmountInCart = items.reduce((acc, item) => {
+    return acc + item.amount;
+  }, 0);
+
   return (
     <HeaderContainer>
       <NavLink to="/">
@@ -17,7 +25,7 @@ export function Header() {
           Adamantina, SP
         </Location>
 
-        <CheckoutButton to="/checkout">
+        <CheckoutButton amountInCart={itemsAmountInCart} to="/checkout">
           <ShoppingCart weight="fill" />
         </CheckoutButton>
       </div>
--- Desafio02-CoffeeDelivery/src/components/Header/styles.ts
+++ Desafio02-CoffeeDelivery/src/components/Header/styles.ts
@@ -1,8 +1,12 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const HeaderContainer = styled.header`
   padding: 2rem 10rem;
+  background: ${(props) => props.theme.background};
+  position: sticky;
+  top: 0px;
+  width: 100%;
 
   display: flex;
   justify-content: space-between;
@@ -32,14 +36,45 @@ export const Location = styled.div`
   }
 `;
 
-export const CheckoutButton = styled(NavLink)`
+interface CheckoutButtonProps {
+  amountInCart: number;
+}
+
+export const CheckoutButton = styled(NavLink)<CheckoutButtonProps>`
+  position: relative;
   background: ${(props) => props.theme["yellow-light"]};
   color: ${(props) => props.theme["yellow-dark"]};
   border-radius: 6px;
   padding: 0.625rem;
 
+  display: flex;
+
   svg {
     height: 1.375rem;
     width: 1.375rem;
   }
+
+  ${(props) =>
+    props.amountInCart
+      ? css`
+          &::after {
+            content: "${props.amountInCart}";
+            position: absolute;
+            right: 0;
+            top: 0;
+            transform: translate(50%, -50%);
+            background: ${(props) => props.theme["yellow-dark"]};
+            color: ${(props) => props.theme.white};
+            height: 1.25rem;
+            width: 1.25rem;
+            border-radius: 999px;
+            font-size: 0.75rem;
+            font-weight: 700;
+
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+        `
+      : ""}
 `;
