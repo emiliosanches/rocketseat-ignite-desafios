05c8a9b59f92cfbfce625d51598ba1351c8824df
Emilio <emiliosneto13@gmail.com>
2023-01-06 23:23:26 -0300

    CoffeeDelivery: Implement order success page

--- Desafio02-CoffeeDelivery/src/pages/Success/index.tsx
+++ Desafio02-CoffeeDelivery/src/pages/Success/index.tsx
@@ -1,3 +1,95 @@
+import { CurrencyDollar, MapPin, Timer } from "phosphor-react";
+import { Location, useLocation } from "react-router-dom";
+
+import deliveryIllustration from "../../assets/delivery-illustration.svg";
+
+import {
+  IconContainer,
+  OrderInfoCard,
+  OrderInfoLine,
+  SuccessPageCardsContainer,
+  SuccessPageContainer,
+  SuccessPageSubtitle,
+  SuccessPageTitle,
+} from "./styles";
+
+const paymentTypesLabels = {
+  credit_card: "Cartão de Crédito",
+  debit_card: "Cartão de Débito",
+  money: "Dinheiro",
+};
+
+interface ILocation extends Location {
+  state: {
+    address: {
+      street: string;
+      number: string;
+      neighborhood: string;
+      city: string;
+      state: string;
+    };
+    payment_method: "credit_card" | "debit_card" | "money";
+  };
+}
+
 export function Success() {
-  return <h2>Success</h2>;
+  const { state } = useLocation() as ILocation;
+
+  return (
+    <SuccessPageContainer>
+      <SuccessPageTitle>Uhu! Pedido confirmado</SuccessPageTitle>
+      <SuccessPageSubtitle>
+        Agora é só aguardar que logo o café chegará até você
+      </SuccessPageSubtitle>
+
+      <SuccessPageCardsContainer>
+        <OrderInfoCard>
+          <OrderInfoLine>
+            <IconContainer backgroundColor="purple">
+              <MapPin weight="fill" />
+            </IconContainer>
+            <div>
+              <div>
+                Entrega em{" "}
+                <strong>
+                  {state.address.street}, {state.address.number}
+                </strong>
+              </div>
+              <div>
+                {state.address.neighborhood} - {state.address.city},{" "}
+                {state.address.state}
+              </div>
+            </div>
+          </OrderInfoLine>
+          <OrderInfoLine>
+            <IconContainer backgroundColor="yellow">
+              <Timer weight="fill" />
+            </IconContainer>
+            <div>
+              <div>Previsão de entrega</div>
+              <div>
+                <strong>20 min - 30 min</strong>
+              </div>
+            </div>
+          </OrderInfoLine>
+          <OrderInfoLine>
+            <IconContainer backgroundColor="yellow-dark">
+              <CurrencyDollar weight="fill" />
+            </IconContainer>
+            <div>
+              <div>Pagamento na entrega</div>
+              <div>
+                <strong>{paymentTypesLabels[state.payment_method]}</strong>
+              </div>
+            </div>
+          </OrderInfoLine>
+        </OrderInfoCard>
+
+        <img
+          src={deliveryIllustration}
+          alt="Ilustração de um entregador de pedidos"
+        />
+      </SuccessPageCardsContainer>
+    </SuccessPageContainer>
+  );
 }
